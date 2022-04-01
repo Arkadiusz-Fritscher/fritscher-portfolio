0ab88f827b7a56ea88c973af87374d06025e06af
Arek <arkadiuszp@arcor.de>
2022-04-01 12:59:15 +0200

    feature: adding simple head/meta in project template

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -24,6 +24,37 @@ export default {
       project: null,
     };
   },
+
+  head() {
+    return {
+      title: this.project.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.project.description,
+        },
+        // Open Graph
+        { hid: 'og:title', property: 'og:title', content: this.project.title },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: this.project.description,
+        },
+        // Twitter Card
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: this.project.title,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: this.project.description,
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -32,6 +63,7 @@ export default {
     <pre>
       {{ project }}
     </pre>
+    <nuxt-content :document="project"> </nuxt-content>
   </div>
 </template>
 
